b2d42e6983a8f5a32aa595f6a6b4a69bf54542ba
Amelle Margaron <a_marga@cslab-pluto.concordia.ca>
2020-01-30 15:21:50 -0500

    P1: added basic Jquery + jquey ui script et

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -2,10 +2,17 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
+Project 1
+Amelle Margaron
 
 This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+
+$(document).ready(setup);
+
+function setup() {
+  // This code will run when the document is ready!
+}
