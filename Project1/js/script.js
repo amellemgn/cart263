0de98fd9d147e93474221f0c2186a83683a1a47f
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-01-30 16:57:41 -0500

    P1: Added draft vultures + and tested drag + drop liver (works!?)

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -10,9 +10,36 @@ to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
 
-
+let $vulture;
+let $liver;
 $(document).ready(setup);
 
 function setup() {
-  // This code will run when the document is ready!
+  // code runs when page loads
+  // get all liver objets (just one, could've used id here) + vulture objects
+  $liver = $('.liver');
+  $vulture = $('.vulture');
+  // make vultures droppable
+  $vulture.droppable({
+    drop: onDrop
+  });
+  //make liver draggable
+  $liver.draggable({
+    //nothing here right now, what do i want to happend when liver starts or stops being dropped?
+  });
+}
+//
+function onDrop(event, ui){
+  ui.draggable.remove();
+  setTimeout(revert, 250);
+}
+
+function revert(){
+  console.log("revert");
+  let $liver = document.createElement('div');
+  $liver.setAttribute('class', 'liver');
+  let $prometheus = $('.prometheus');
+  $prometheus.append($liver);
+  //$('.prometheus').append($liver);
+  $liver.draggable({});
 }
