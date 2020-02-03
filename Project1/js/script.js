6a3b7ab990156cad40346afcc0b54976455f54e7
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-02-03 16:57:36 -0500

    P1: Added button + hover, tried vulture hover, added containment to liver

--- Project1/js/script.js
+++ Project1/js/script.js
@@ -11,13 +11,18 @@ to match your project! Write JavaScript to do amazing things below!
 Mostly combines code from various in-class exercises.
 
 *********************************************************************/
+//Ask pippin: resistance
 
 // Sabine questions: creating pull resistance: like slowing down frame rate?
 // v2 isn't working
 //random isn't ~feeling like super random : rule that if you get the same vulture twice your third has to be different
 // or just: variable that saves last vulture: if last vulture = new vulture randomize again
+
+//background color: one of those color animations
 let $vulture;
 let $liver;
+let $button;
+
 const UPDATE_FREQUENCY = 20;
 const REVEAL_POSSIBILITY1 = 0.3;
 const REVEAL_POSSIBILITY2 = 0.6;
@@ -35,41 +40,68 @@ function setup() {
   selectedVulture = $('#v1');
   $liver = $('.liver');
   $vulture = $('.vulture');
+  $button = $('.button');
   // make vultures droppable
   $vulture.droppable({
-    drop: function(event, ui){
+    drop: function(event, ui) {
       ui.draggable.remove();
       createAnew();
     }
   });
+
   //make liver draggable
-  $liver.draggable({});
+  $liver.draggable({containment: (".box"), scroll: false});
   setInterval(callBackBounce, 1000);
+  //  .hover (what happens mousein, what happens mouseout)
+  $button.hover(function() {
+    $button.css("background-color", "pink");
+  }, function() {
+    $button.css("background-color", "yellow");
+  })
+  $button.one('mousedown', clickButton);
+  $('.big').hover(function() {
+    $(this).css("width", 250 + "px");
+  }, function() {
+    $button.css("width", 200 + "px");
+  });
+  $vulture.hover(function() {
+    $(this).css("color", "transparent");
+  }, function() {
+    $button.css("color", "yellow");
+  });
+
 }
 
-function startMusic(){
+function startMusic() {
   BITE_SOUND.play();
   BIRD_SOUND.play();
 }
 
-function createAnew(){
+function createAnew() {
   let newDiv = $('<div>');
   $(newDiv).attr("class", "liver");
   $(newDiv).draggable();
   $(".prometheus").append(newDiv);
 
 
-    let r = Math.random();
-    console.log(r);
-    if (r < REVEAL_POSSIBILITY1) { // what gets passed through here
-      selectedVulture = $('#v1');
-    } else if ( REVEAL_POSSIBILITY1 < r < REVEAL_POSSIBILITY2) {
-      selectedVulture = $('#v2');
-    } else {
-      selectedVulture = $('#v3');
-    }
+  let r = Math.random();
+  console.log(r);
+  if (r < REVEAL_POSSIBILITY1) { // what gets passed through here
+    selectedVulture = $('#v1');
+  } else if (REVEAL_POSSIBILITY1 < r < REVEAL_POSSIBILITY2) {
+    selectedVulture = $('#v2');
+  } else {
+    selectedVulture = $('#v3');
+  }
 }
 
-function callBackBounce(){
-  selectedVulture.effect("bounce", {direction: 'down', times:3}, 500);
+function callBackBounce() {
+  selectedVulture.effect("bounce", {
+    direction: 'down',
+    times: 3
+  }, 500);
+}
+
+function clickButton() {
+  console.log("button clicked");
 }
