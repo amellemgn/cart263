2995a839d18d5b9302f14e20b56e07935769e940
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-03-03 16:15:21 -0500

    P2: Added possibility of asking voice to repeat statement. Added music on click

--- Project2Test/js/script.js
+++ Project2Test/js/script.js
@@ -18,67 +18,91 @@ let angle = 0;
 let $chosenGif;
 let randomizedSpeech;
 let lastRandomizedSpeech;
-let mouseX =0;
+let mouseX = 0;
 let mouseY = 0;
 let colors;
 let animateLoop;
-let i =0;
+let i = 0;
 const ANIMATION_TIME = 500;
+let $commentTrigger;
+let selectedComment;
+let commentArray;
+let closeGifCounter= 0;
 
 $(document).ready(setup);
 
 function setup() {
-  backgroundMusic.loop = true;
-  backgroundMusic.play();
-  backgroundMusic.volume = 0.2;
+  //Set the dialog widgets not to open upon initialization
+ $('#comment1, #comment2, #comment3').dialog(
+   {autoOpen: false},
+   {position: {my: "left top", at:"center", of: "window"}}
+ );
+ // Math.random()*($(document).width())
+ $(document).one('mousedown', startMusic);
+
   $chosenGif = $('.chosenGif');
   $carousel = $('.carousel');
+  $commentTrigger = $('.commentTrigger');
+  $commentTrigger.hover(
+    function() {
+      $(this).css("background-color", "yellow");
+    },
+    function() {
+      $(this).css({
+        "background-color": ''
+      });
+    });
   $gif = $('.gif');
   $gif.hover(
-         function()
-         {
-           var src = $(this).attr("src");
-           $(this).attr("src", src.replace(/\.png$/i, ".gif"));
-         },
-         function()
-         {
-           var src = $(this).attr("src");
-           $(this).attr("src", src.replace(/\.gif$/i, ".png"));
-         });
+    function() {
+      var src = $(this).attr("src");
+      $(this).attr("src", src.replace(/\.png$/i, ".gif"));
+    },
+    function() {
+      var src = $(this).attr("src");
+      $(this).attr("src", src.replace(/\.gif$/i, ".png"));
+    });
   setInterval(updateCarousel, 100);
   setInterval(gaugeMousePosition, 100);
   $(document).on('mousemove', updateMouse);
 
   $gif.on('click', gifClicked);
+  $commentTrigger.on('click', commentTriggerClicked);
   callAnnyang();
   // Background animation
-   // Using remainder formula, cycle through array of background colors. Use animate effect to ensure smooth transition.
-   // Code from - http://jsfiddle.net/bHEVr/
-   colors = ["#FDDFDF", "#FCF7DE", "#DEFDE0", "#DEF3FD", "#F0DEFD"];
-   animateLoop = function() {
-     $('.background').animate({
-       backgroundColor: colors[i++ % colors.length] // find the remainder of
-     }, ANIMATION_TIME, function() {
-       animateLoop();
-     });
-   };
-   animateLoop();
+  // Using remainder formula, cycle through array of background colors. Use animate effect to ensure smooth transition.
+  // Code from - http://jsfiddle.net/bHEVr/
+  colors = ["#FDDFDF", "#FCF7DE", "#DEFDE0", "#DEF3FD", "#F0DEFD"];
+  animateLoop = function() {
+    $('.background').animate({
+      backgroundColor: colors[i++ % colors.length] // find the remainder of
+    }, ANIMATION_TIME, function() {
+      animateLoop();
+    });
+  };
+  animateLoop();
 }
-function updateMouse(e){
-mouseX = e.pageX;
-mouseY = e.pageY;
+
+function startMusic(){
+  backgroundMusic.loop = true;
+  backgroundMusic.play();
+  backgroundMusic.volume = 0.2;
+}
+
+function updateMouse(e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
 }
 
 function updateCarousel() {
   $carousel.css("transform", 'translateZ(-288px) rotateY(' + angle + 'deg)');
 }
 
-function gaugeMousePosition(e){
-  if(mouseX < 450){
+function gaugeMousePosition(e) {
+  if (mouseX < 450) {
     angle += 2;
-  }
-  else{
-      angle -= 2;
+  } else {
+    angle -= 2;
   }
 }
 
@@ -86,6 +110,7 @@ function callAnnyang() {
   if (annyang) {
     console.log("annyang");
     var command = {
+      "Repeat": repeatSpeech,
       "Me": closeGif,
       "I am the *problem": closeGif,
       "yes": closeGif,
@@ -103,17 +128,17 @@ function gifClicked() {
   automatedSpeech();
 }
 
-function largeGifAppears(e){
-console.log("large gif appears");
-console.log(e.src);
-let src = $(this).attr('src');
-$chosenGif.attr("src", src); // can i do this
-$chosenGif.show();
-// efffects
-// $chosenGif.css("display", "in-line");
+function largeGifAppears(e) {
+  console.log("large gif appears");
+  console.log(e.src);
+  let src = $(this).attr('src');
+  $chosenGif.attr("src", src); // can i do this
+  $chosenGif.show();
+  // efffects
+  // $chosenGif.css("display", "in-line");
 }
 
-function automatedSpeech(){
+function automatedSpeech() {
   console.log("automated speech");
   selectSpeech();
   let randomPitch, randomRate = Math.random();
@@ -124,29 +149,46 @@ function automatedSpeech(){
   responsiveVoice.speak(randomizedSpeech, "UK English Male", options)
 }
 
-function selectSpeech(){
+function repeatSpeech(){
+  let randomPitch, randomRate = Math.random();
+  let options = {
+    rate: randomRate,
+    pitch: randomPitch
+  };
+  responsiveVoice.speak(randomizedSpeech, "UK English Male", options)
+}
+
+function selectSpeech() {
   let r = Math.random();
   console.log(r);
-  if (r < 0.2){
+  if (r < 0.2) {
     randomizedSpeech = "do you not think this could be qualified as a matter of perception";
-  }
-  else if ( r < 0.4){
+  } else if (r < 0.4) {
     randomizedSpeech = "do the children at least have the excuse of being gullible";
-  }
-  else if (  r < 0.6){
+  } else if (r < 0.6) {
     randomizedSpeech = "in the war for attention is this not a just cause";
-  }
-  else if ( r < 0.8){
+  } else if (r < 0.8) {
     randomizedSpeech = "is your fear of the mouse cynical";
-  }
-  else {
+  } else {
     randomizedSpeech = "are you aware or are you sick";
     // say booger three times
   }
 }
 
-function closeGif(){
+function closeGif() {
   console.log("close gif");
-   $chosenGif.hide();
-  // in this one .. i dont need to be using e or this right?
+  $chosenGif.hide();
+  console.log(closeGifCounter);
+  closeGifCounter +=1;
+  // make some cute draggable things appear
+}
+
+function commentTriggerClicked() {
+  setInterval(callComment, 500);
+}
+
+function callComment() {
+  commentArray = [$('#comment1'), $('#comment2'), $('#comment3'), $('#comment4')];
+  selectedComment = commentArray[Math.floor(Math.random() * commentArray.length)];
+  selectedComment.dialog('open');
 }
