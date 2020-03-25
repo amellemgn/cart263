7715c69e513437b4abddcd3e8af876417d04411a
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-03-25 19:24:52 -0400

    A4: Commenting completed

--- Assignment4/js/script.js
+++ Assignment4/js/script.js
@@ -1,55 +1,61 @@
-$(document).ready(setup);
+//Declare relevant variables
 let $refresh;
 let randomCondiment;
 let randomCat;
 let randomRoom;
 let randomWrestler;
 let randomMove;
-
-
+// Calls setup when page loads
+$(document).ready(setup);
+//Setup calls JSON data which we will use to randomly select words.
 function setup() {
   $.getJSON('data/data.json')
-    .done(gotData)
-    .fail(dataError);
-
+    .done(gotData) // calls this function when done loading data
+    .fail(dataError); // calls this function if data loads erroneously
+//Create a refresh button and allow it to basically act as an equivalent to clicking the mouse anywhere onscreen
   $refresh = $('.refresh');
-  $refresh.on("click", gotData);
+  $refresh.on("click", mousePressed);
 }
-
+//gotData is called when the JSON data has loaded
+//randomizes words for each part  of our phrase and assembles the phrase to display
 function gotData(data) {
-  console.log("got data");
+  //randomize condiment fron JSON data
   randomCondiment = getRandomElement(data.condiments);
+  // the verb 'is' follows our condiment. if the condiment is a plural noun, conjugate to 'are'
   let verb = 'is';
   if (randomCondiment.charAt(randomCondiment.length - 1) === 's') {
     verb = 'are';
   }
-
+//randomize cat from JSON cat data
   randomCat = getRandomElement(data.cats);
-
+// indefinite article precedes cat. make an array containing vowel, and if the first letter of the
+// random cat matches a vowel, change indefinite article to make a grammatically correct phrase
   let indefiniteArticle = 'a';
-  // case sensitive
-  let vowels = ["A", "E", "I", "O", "U"];
+  let vowels = ["A", "E", "I", "O", "U"]; // case sensitive
   for (let i = 0; i < vowels.length; i++) {
     console.log(randomCat.charAt(randomCat[0]));
     if (randomCat.charAt(randomCat[0]) === vowels[i]) {
-        indefiniteArticle = 'an';
-      }
+      indefiniteArticle = 'an';
     }
-
-    randomRoom = getRandomElement(data.rooms);
-    randomWrestler = getRandomElement(data.wrestlers);
-    randomMove = getRandomElement(data.moves);
-
-    let description = `${randomCondiment} ${verb} like ${indefiniteArticle} ${randomCat} in a ${randomRoom}, held in the delicate hands of ${randomWrestler} seconds before a lethal ${randomMove}.`;
-
-    $('body').append(description)
   }
-
-
-  function dataError(request, text, error) {
-    console.error(error);
-  }
-
-  function getRandomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
+//randomize room, wrestler, wrestling move based on JSON data.
+  randomRoom = getRandomElement(data.rooms);
+  randomWrestler = getRandomElement(data.wrestlers);
+  randomMove = getRandomElement(data.moves);
+// create descrioption with our randomized words and (adjusted) grammar and append to html body
+  let description = `${randomCondiment} ${verb} like ${indefiniteArticle} ${randomCat} in a ${randomRoom}, held in the delicate hands of ${randomWrestler} seconds before a lethal ${randomMove}.`;
+  $('body').append(description)
+}
+// Show error message in console if loading JSON data malfunctions
+function dataError(request, text, error) {
+  console.error(error);
+}
+// With JSON data sent as an array into this function, select and return a random element
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+// The equivalent of a 'refresh' button. Clicking reloads the web page, generating new words
+function mousePressed() {
+  console.log("pressed");
+  location.reload(true);
+}
