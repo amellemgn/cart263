39a9dd623e0b6e928aec99b506391d2a36410a11
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-03-25 18:52:45 -0400

    A4: fixed changing indefinite article

--- Assignment4/js/script.js
+++ Assignment4/js/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(setup);
 let $refresh;
 let randomCondiment;
@@ -8,7 +7,7 @@ let randomWrestler;
 let randomMove;
 
 
-function setup(){
+function setup() {
   $.getJSON('data/data.json')
     .done(gotData)
     .fail(dataError);
@@ -19,33 +18,38 @@ function setup(){
 
 function gotData(data) {
   console.log("got data");
-   randomCondiment = getRandomElement(data.condiments);
+  randomCondiment = getRandomElement(data.condiments);
   let verb = 'is';
   if (randomCondiment.charAt(randomCondiment.length - 1) === 's') {
     verb = 'are';
   }
 
- randomCat = getRandomElement(data.cats);
+  randomCat = getRandomElement(data.cats);
 
- let indefiniteArticle ='a';
- if(randomCat.charAt(randomCat[0]=== 'a' || randomCat.charAt(randomCat[0]=== 'o'|| 'e'|| 'u' || 'i')){
-   indefiniteArticle = 'an';
- }
+  let indefiniteArticle = 'a';
+  // case sensitive
+  let vowels = ["A", "E", "I", "O", "U"];
+  for (let i = 0; i < vowels.length; i++) {
+    console.log(randomCat.charAt(randomCat[0]));
+    if (randomCat.charAt(randomCat[0]) === vowels[i]) {
+        indefiniteArticle = 'an';
+      }
+    }
 
- randomRoom = getRandomElement(data.rooms);
- randomWrestler = getRandomElement(data.wrestlers);
- randomMove = getRandomElement(data.moves);
+    randomRoom = getRandomElement(data.rooms);
+    randomWrestler = getRandomElement(data.wrestlers);
+    randomMove = getRandomElement(data.moves);
 
-  let description = `${randomCondiment} ${verb} like ${indefiniteArticle} ${randomCat} in a ${randomRoom}, held in the delicate hands of ${randomWrestler} seconds before a lethal ${randomMove}.`;
+    let description = `${randomCondiment} ${verb} like ${indefiniteArticle} ${randomCat} in a ${randomRoom}, held in the delicate hands of ${randomWrestler} seconds before a lethal ${randomMove}.`;
 
-  $('body').append(description)
-}
+    $('body').append(description)
+  }
 
 
-function dataError(request, text, error) {
-  console.error(error);
-}
+  function dataError(request, text, error) {
+    console.error(error);
+  }
 
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
